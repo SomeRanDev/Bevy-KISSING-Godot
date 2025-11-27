a64e66d00b987dc8a304b5c2c3e207707d955477
Maybee Rezbit <git@someran.dev>
2025-11-26 21:46:03 -0500

    Add process/physics_process macro wrapper arguments

--- bevy_kissing_godot_macros/src/lib.rs
+++ bevy_kissing_godot_macros/src/lib.rs
@@ -21,6 +21,46 @@ pub fn get_compilation_timestamp(input: TokenStream) -> TokenStream {
 	get_compilation_timestamp::get_compilation_timestamp_impl(input)
 }
 
+/// Used to mark the entry function for a Bevy💋Godot app.
+///
+/// This attribute requires one argument for the name of the Bevy app node that needs
+/// to be added as an autoload in the editor:
+/// ```rust
+/// #[kiss_bevy(MyAppNodeName)]
+/// fn main(app: &mut bevy::prelude::App) {
+///		// Do stuff with `app`...
+/// }
+/// ```
+///
+/// The second and third arguments are optional. If provided, they are both expected to
+/// be paths to macros that take two expression arguments. The "second" is a macro that
+/// wraps the "process" expression of the generated Bevy app node. The "third" is the
+/// same, but it's for the "physics_process" expression.
+///
+/// These macros should take two arguments:
+/// 	* The first is the original expression
+/// 	* The second is the `self` expression
+///
+/// ```rust
+/// macro_rules panic_catcher {
+/// 	($process: expr, $self: expr) => {
+///			let result = std::panic::catch_unwind(|| {
+/// 			$process
+/// 		});
+/// 		if result.is_err() {
+/// 			println!("Panic happened!");
+///
+/// 			// Check bevy_kissing_godot::kissing_app for all `self.app` functions.
+/// 			$self.app.clear_app();
+/// 		}
+/// 	}
+/// }
+///
+/// #[kiss_bevy(MyAppNodeName, panic_catcher, panic_catcher)]
+/// fn main(app: &mut bevy::prelude::App) {
+///		// Do stuff with `app`...
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn kiss_bevy(attr: TokenStream, item: TokenStream) -> TokenStream {
 	kiss_bevy::kiss_bevy_impl(attr, item)
