5822e36f8d4c1120deb140d00b904bb5f8190cda
Maybee Rezbit <28355157+SomeRanDev@users.noreply.github.com>
2026-02-03 06:26:23 -0500

    Use GodotNodeId more consistently internally

--- bevy_kissing_godot/src/components/godot_node_id.rs
+++ bevy_kissing_godot/src/components/godot_node_id.rs
@@ -3,7 +3,7 @@ use godot::prelude::*;
 
 use crate::resources::all_nodes::AllNodes;
 
-#[derive(Component)]
+#[derive(Component, Debug)]
 pub struct GodotNodeId {
 	id: usize,
 }
@@ -15,5 +15,11 @@ impl GodotNodeId {
 
 	pub fn get_as<T: Inherits<Node>>(&self, all_nodes: &AllNodes) -> Gd<T> {
 		all_nodes.get(self.id).try_cast::<T>().unwrap()
+	}
+
+	pub fn try_get_as<T: Inherits<Node>>(&self, all_nodes: &AllNodes) -> Option<Gd<T>> {
+		all_nodes
+			.try_get(self.id)
+			.and_then(|n| n.try_cast::<T>().ok())
 	}
 }
