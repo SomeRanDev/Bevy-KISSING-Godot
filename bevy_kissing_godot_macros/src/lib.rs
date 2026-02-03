b28487ec4847fa895f2151b89afa1f17753c1a79
Maybee Rezbit <git@someran.dev>
2026-02-03 06:19:55 -0500

    Add doc comments for ALL top-level macros

--- bevy_kissing_godot_macros/src/lib.rs
+++ bevy_kissing_godot_macros/src/lib.rs
@@ -18,11 +18,65 @@ mod utils;
 // * Functions *
 // -------------
 
+/// Used to expose a Bevy component to the Godot editor.
+///
+/// This should be used alongside `#[derive(bevy::prelude::Component, bevy_kissing_godot::prelude::KissingComponent)]`.
+/// ```rust
+/// #[derive(Component, KissingComponent)]
+/// #[kissing_component]
+/// struct Health {
+/// 	maximum: i32,
+/// }
+/// ```
+///
+/// To enable a field to be modified in the Godot editor, the `#[export]` attribute can be used.
+/// Optionally, a default-value can be provided with `initial_value = VALUE`.
+/// ```rust
+/// #[derive(Component, KissingComponent)]
+/// #[kissing_component]
+/// struct Health {
+/// 	#[export(initial_value = 100)]
+/// 	maximum: i32,
+/// }
+/// ```
+///
+/// `#[export_node]` can be used to allow for a `NodePath` input.
+///
+/// To allow only certain classes to be selected in the editor, Godot classes may be listed
+/// as the arguments to the attribute:
+/// ```rust
+/// #[export_node(Camera3D, CollisionShape3D, Path3D)]
+/// ```
+///
+/// A field with `#[export_node]` MUST be an `Option<bevy_kissing_godot::prelude::GodotNodeId>`.
+/// `GodotNodeId` can be converted an actual `Gd<T>` node through `NonSend<AllNodes>` at runtime.
+/// ```rust
+/// #[derive(Component, KissingComponent)]
+/// #[kissing_component]
+/// struct Health {
+/// 	#[export(initial_value = 100)]
+/// 	maximum: i32,
+///
+/// 	#[export_node(Label)]
+/// 	label: Option<GodotNodeId>,
+/// }
+///
+/// fn on_update(
+///		healths: Query<&Health>,
+///		all_nodes: NonSend<AllNodes>,
+/// ) {
+/// 	for health in gooblers.iter() {
+/// 		let mut health_label: Gd<Label> = health.label.get_as::<Label>(&all_nodes);
+/// 	}
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn kissing_component(attr: TokenStream, item: TokenStream) -> TokenStream {
 	kissing_component::kissing_component_impl(attr, item)
 }
 
+/// A utility used to track whether the Rust binary has been recompiled in Godot.
+/// See `kissing_component::kissing_component_registry::KissingComponentRegistry::get_compilation_id`.
 #[proc_macro]
 pub fn get_compilation_timestamp(input: TokenStream) -> TokenStream {
 	get_compilation_timestamp::get_compilation_timestamp_impl(input)
@@ -73,21 +127,51 @@ pub fn kiss_bevy(attr: TokenStream, item: TokenStream) -> TokenStream {
 	kiss_bevy::kiss_bevy_impl(attr, item)
 }
 
+/// This should be added to all custom Godot types added in Rust so they will appear in the hierarchy.
+/// NOTE: Is there any difference between this and `#[derive(KissingNode)]`??? Why did I add both???
 #[proc_macro]
 pub fn kiss_node(input: TokenStream) -> TokenStream {
 	kiss_node::kiss_node_impl(input)
 }
 
+/// Generates functions and metadata to allow for a Bevy component to be accessible from the Godot editor.
+/// This should be used with `#[kissing_component]`.
 #[proc_macro_derive(KissingComponent)]
 pub fn kissing_component_derive(input: TokenStream) -> TokenStream {
 	kissing_component_derive::kissing_component_derive_impl(input)
 }
 
+/// This should be added to all custom Godot types added in Rust so they will appear in the hierarchy.
+/// NOTE: Is there any difference between this and `#[kiss_node]`??? Why did I add both???
 #[proc_macro_derive(KissingNode)]
 pub fn kissing_node_derive(input: TokenStream) -> TokenStream {
 	kissing_node_derive::kissing_node_derive_impl(input)
 }
 
+/// Given a function that takes an `app: &mut bevy::prelude::App`, this generates an empty
+/// Bevy component AND a `bevy::prelude::Plugin` that runs the function as its `build` function.
+///
+/// This code...
+/// ```rust
+/// #[plugin_and_kissing_component(Cool)]
+/// pub(crate) fn cool_plugin(app: &mut App) {
+/// 	app.add_systems(Startup, init_cool_stuff);
+/// }
+/// ```
+///
+/// ...gets converted to this.
+/// ```rust
+/// #[kissing_component]
+/// #[derive(Component, KissingComponent)]
+/// struct CoolComponent;
+///
+/// pub(crate) struct CoolPlugin;
+/// impl Plugin for CoolPlugin {
+/// 	fn build(&self, app: &mut App) {
+/// 		app.add_systems(Startup, init_cool_stuff);
+/// 	}
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn plugin_and_kissing_component(attr: TokenStream, item: TokenStream) -> TokenStream {
 	plugin_and_kissing_component::plugin_and_kissing_component_impl(attr, item)
